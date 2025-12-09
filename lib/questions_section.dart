5196f7f84875e8086e9830a6c9fc87d2cac8f28a
SAary001 <aarysatardekar2003@gmail.com>
2025-12-10 04:50:49 +0530

    fix: Correct formatting and indentation in QuestionsSection widget

--- lib/questions_section.dart
+++ lib/questions_section.dart
@@ -11,7 +11,14 @@ class QuestionsSection extends StatelessWidget
   {
     return Scaffold
     (
-
+      appBar: AppBar
+      (
+          title: Text('Quiz App', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 69, 56, 18)),),
+        ),
+        body: Center
+        (
+          child: Text("This is the Questions Section", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+        )   ,
     );
   }
 }
